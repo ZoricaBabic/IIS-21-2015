1b803ce4053b6b1179c40857ad5740329ad00662
ZoricaBabic <zorica.b96@gmail.com>
2018-03-24 16:00:05 +0100

    Last commit

--- IT 21-2015 Babic Zorica/src/cmd/CmdUndoRedo.java
+++ IT 21-2015 Babic Zorica/src/cmd/CmdUndoRedo.java
@@ -7,130 +7,198 @@ import crtanje.Model;
 import geometrija.Oblik;
 
 public class CmdUndoRedo implements Command {
-	
+
 	private ArrayList<Oblik> commands = new ArrayList<Oblik> ();
 	private int currentPostition = -1;
 	private Model model = new Model();
 	private CmdAddShape cmdAddShape;
-	
+
 	public CmdUndoRedo() {
-		
+
 	}
-	
+
 	public CmdUndoRedo(Model model) {
-		
+
 		this.model = model;
 	}
-	
+
 	public void add(Oblik s) {
-		
+
 		commands.add(s);
 		currentPostition++;
-		
+
 		boolean exists = false;
-		
+
 		if(model.getListaObjekata().isEmpty() == false) {
-			
+
 			for(int i = 0; i<model.getListaObjekata().size(); i++) {
-				
+
 				if(model.getListaObjekata().get(i).equals(s)) {
-					
+
 					exists = true;
-					
-					
+
+
 				}
-				
-				
+
+
 			}
 		} 
-		
+
 		if(exists == false) {
-			
+
 			cmdAddShape = new CmdAddShape(model,s);
 			cmdAddShape.execute();
-			//currentPostition = commands.size()-1;
+
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 	}
-	
+
 	public void remove(Oblik s) {
-		
+
 		commands.remove(s);
 	}
-	
+
 
 	@Override
 	public void execute() {
-		
-		
-		
-		/*cmdAddShape.execute();
-		currentPostition++;
-		cmdAddShape.setShape(commands.get(currentPostition+1));*/
-		
-		
-		
-		
+
+		if(currentPostition < commands.size()-1 ) {
+			
+			//imam sledeci
+			
+			if(currentPostition != -1) {
+				
+				if(commands.get(currentPostition).equals(commands.get(currentPostition+1)) == false){
+					
+					cmdAddShape = new CmdAddShape(model,commands.get(currentPostition+1));
+					cmdAddShape.execute();
+					currentPostition++;
+					
+					
+				} else {
+					
+					//modfikacija
+				}
+				
+				
+			} else {
+				
+				cmdAddShape = new CmdAddShape(model,commands.get(currentPostition+1));
+				cmdAddShape.execute();
+				currentPostition++;
+				
+				
+			}
+			
+			
+		} else {
+			
+			
+		}
+
+
+
+
+
 	}
 
 	@Override
 	public void unexecute() {
-		
-		currentPostition--;
-		
-		if(model.getListaObjekata().size() > 1) {
-			
-			if(commands.get(currentPostition).equals(commands.get(currentPostition+1)) == false) {
+
+		System.out.println("Current: " + currentPostition);
+
+		if(currentPostition >  0) {
+
+
+			if(commands.get(currentPostition).equals(commands.get(currentPostition-1)) == false) {
+
+				//treba da se obriše trentuni i smanji se current posititon 
+				//setujem oblik na trenutni
 				
 				cmdAddShape.unexecute();
+				currentPostition--;
 				cmdAddShape.setShape(commands.get(currentPostition));
+				
+			} else {
+
+				System.out.println("Modifikacija!");
 			}
+
 			
-		} else if(model.getListaObjekata().size() == 1) {
+
+		} else {
 			
 			cmdAddShape.unexecute();
-		}
-		
-		/*currentPostition--;
-		
-		if(model.getListaObjekata().size() > 1) {
+			currentPostition = -1;
 			
+			
+
+
+		}
+
+
+		/*if(currentPostition > 0) {
+
+			if(commands.get(currentPostition).equals(commands.get(currentPostition-1)) == false) {
+
+				cmdAddShape.unexecute();
+				currentPostition--;
+				cmdAddShape.setShape(commands.get(currentPostition));
+
+			} else {
+
+
+				System.out.println("Modifikacija!");
+			}
+
+		} else if(currentPostition == 0) {
+
+			cmdAddShape.unexecute();
+			currentPostition--;
+
+
+		}*/
+
+		/*currentPostition--;
+
+		if(model.getListaObjekata().size() > 1) {
+
 			if(commands.get(currentPostition).equals(commands.get(currentPostition+1)) == false){
-				
+
 				cmdAddShape.unexecute();
 				cmdAddShape.setShape(commands.get(currentPostition));
 
 			}
-			
-			
+
+
 		} else if(model.getListaObjekata().size() > 0) {
-			
+
 			cmdAddShape.unexecute();
 			cmdAddShape.setShape(commands.get(currentPostition));
-			
+
 		}*/
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 		//cmdAddShape.unexecute();
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 	}
 
 }
